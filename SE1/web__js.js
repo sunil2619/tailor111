76060441e09759bc643bcd5c1ae152656abc1d1c
sunil2619 <84694591+sunil2619@users.noreply.github.com>
2022-01-03 17:04:40 +0530

    Add files via upload

---
+++ SE1/web__js.js
@@ -0,0 +1,17 @@
+alert("Shut the F up");
+
+document.write("<h2>Hello There :)</h2>");
+document.write("<hr>");
+document.write("This is JS tutorial.");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+var name= "Sunil";
+var ocp='programmer';
+
+var age=21;
+var cgpa=6.44;
+
+document.write("My name is " + name);
